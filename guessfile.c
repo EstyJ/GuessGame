aea3bc521116ebda2f5880dad82f7bddc2ee0f88
EstyJ <joshuaesther251@gmail.com>
2023-05-22 15:43:37 +0100

    guessfile

---
+++ guessfile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("#############################");
+	printf("Welcome to my Guesing Game");
+	puts("#############################");
+
+}
