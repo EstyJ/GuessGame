2ebb7c2e61be5f4e1620130ec79fb4c62df2a9a3
EstyJ <joshuaesther251@gmail.com>
2023-05-22 16:34:18 +0100

    updating agin

--- guessfile.c
+++ guessfile.c
@@ -24,14 +24,14 @@ int main(void)
 	if(guessnum == num)
 	{
 		puts("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-		printf("Congratulation you guessed right!!\n");
+		puts("Congratulation you guessed right!!\n");
 		puts("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 	}
 
 	else
 	{
 		puts("**********************");
-		printf("Sorry, Try Harder!!\n");
+		puts("Sorry, Try Harder!!\n");
 		puts("**********************");
 	}
 
