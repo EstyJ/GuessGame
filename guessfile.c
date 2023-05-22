e731a74fba3874b29116f74b4575b886d48db731
N00B-93 <omosheye007@gmail.com>
2023-05-22 16:01:43 +0100

    random number generated

--- guessfile.c
+++ guessfile.c
@@ -2,9 +2,19 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(void)
+{
+
 	puts("#############################");
-	printf("Welcome to my Guesing Game");
+	printf("Welcome to my Guesing Game\n");
 	puts("#############################");
 
+
+	srand(time(NULL));
+
+	int num;
+
+	num = ((rand() % 1000) + 1);
+
+	//printf("%d\n", num);
 }
